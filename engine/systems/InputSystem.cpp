0db016de39a7518794f702d9a597d4aa450a2605
Otavio Capilla <otavio.reis-ext@ab-inbev.com>
2026-02-04 16:52:06 -0300

    feat: refactor InputSystem to separate event polling and update logic

--- engine/systems/InputSystem.cpp
+++ engine/systems/InputSystem.cpp
@@ -1,31 +1,34 @@
 #include "InputSystem.h"
 
-#include <SDL2/SDL.h>
-#include <backends/imgui_impl_sdl2.h>
-
 bool InputSystem::s_quit = false;
 
-void InputSystem::update(){
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        ImGui_ImplSDL2_ProcessEvent(&event);
-        if (event.type == SDL_QUIT) {
-            s_quit = true;
-        }
+bool InputSystem::pollEvent(SDL_Event& event)
+{
+    if (!SDL_PollEvent(&event))
+        return false;
 
-        if(event.type == SDL_KEYDOWN) {
-            if(event.key.keysym.sym == SDLK_ESCAPE) {
-                s_quit = true;
-            }
-        }
-    }
+    if (event.type == SDL_QUIT)
+        s_quit = true;
+
+    if (event.type == SDL_KEYDOWN &&
+        event.key.keysym.sym == SDLK_ESCAPE)
+        s_quit = true;
+
+    return true;
 }
 
-bool InputSystem::quitRequested(){
+void InputSystem::update()
+{
+
+}
+
+bool InputSystem::quitRequested()
+{
     return s_quit;
 }
 
-bool InputSystem::isKeyDown(int scancode) {
+bool InputSystem::isKeyDown(int scancode)
+{
     const Uint8* state = SDL_GetKeyboardState(nullptr);
     return state[scancode];
 }
