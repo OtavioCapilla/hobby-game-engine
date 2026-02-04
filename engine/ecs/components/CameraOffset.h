1dc98be9c80705c329e78cd3e85ba8618adfc929
Otavio Capilla <otavio.reis-ext@ab-inbev.com>
2026-02-04 09:20:10 -0300

    feat: add new ECS component headers for CameraOffset, CameraTarget, Collider, InputIntent, Sprite, Transform, and Velocity

---
+++ engine/ecs/components/CameraOffset.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <engine/math/Vector2.h>
+
+struct CameraOffset
+{
+    Vector2 offset;
+};
---
+++ engine/ecs/components/Velocity.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <engine/math/Vector2.h>
+
+struct Velocity
+{
+    Vector2 value;
+};
