d3aa53fe5bf5b6f786aa8bedb4a9308d393eaa67
Otavio Capilla <otavio.reis-ext@ab-inbev.com>
2026-02-04 16:52:23 -0300

    feat: implement SceneSerializer for saving and loading scene data

---
+++ engine/serialization/SceneSerializer.cpp
@@ -0,0 +1,95 @@
+#include "SceneSerializer.h"
+
+#include <engine/serialization/TilemapSerializer.h>
+#include <engine/serialization/WorldSerializer.h>
+
+#include <engine/vendor/json/json.hpp>
+#include <fstream>
+
+using json = nlohmann::json;
+
+bool SceneSerializer::save(const Scene& scene,
+                           const std::string& path)
+{
+    json root;
+    json sceneJson;
+
+    // =====================
+    // CAMERA
+    // =====================
+    const Camera2D& cam = scene.getCamera();
+
+    sceneJson["camera"] = {
+        { "position", { cam.getPosition().x, cam.getPosition().y } },
+        { "zoom", cam.getZoom() }
+    };
+
+    // =====================
+    // TILEMAP
+    // =====================
+    json tilemapJson;
+    TilemapSerializer::save(scene.getTilemap(), tilemapJson);
+    sceneJson["tilemap"] = tilemapJson;
+
+    // =====================
+    // ECS
+    // =====================
+    json ecsJson;
+    WorldSerializer::save(scene.getWorld(), ecsJson);
+    sceneJson["ecs"] = ecsJson;
+
+    root["scene"] = sceneJson;
+
+    std::ofstream file(path);
+    if (!file.is_open())
+        return false;
+
+    file << root.dump(4);
+    return true;
+}
+
+bool SceneSerializer::load(Scene& scene,
+                           const std::string& path)
+{
+    std::ifstream file(path);
+    if (!file.is_open())
+        return false;
+
+    json root;
+    file >> root;
+
+    const json& sceneJson = root["scene"];
+
+    // =====================
+    // RESET DA SCENE
+    // =====================
+    scene.clear();   // ⚠️ vamos definir esse método
+
+    // =====================
+    // CAMERA
+    // =====================
+    auto camJson = sceneJson["camera"];
+    scene.getCamera().setPosition({
+        camJson["position"][0],
+        camJson["position"][1]
+    });
+    scene.getCamera().setZoom(camJson["zoom"]);
+
+    // =====================
+    // TILEMAP
+    // =====================
+    TilemapSerializer::load(
+        scene.getTilemap(),
+        sceneJson["tilemap"]
+    );
+
+    // =====================
+    // ECS
+    // =====================
+    WorldSerializer::load(
+        scene.getWorld(),
+        sceneJson["ecs"]
+    );
+
+    return true;
+}
