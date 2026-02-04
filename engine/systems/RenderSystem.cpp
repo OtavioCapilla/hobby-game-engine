6a6d85d6ba53aa1313f395d8992a1aaf8c41b539
Otavio Capilla <otavio.reis-ext@ab-inbev.com>
2026-02-04 16:51:54 -0300

    feat: update drawSprite method to use AssetManager for texture retrieval

--- engine/systems/RenderSystem.cpp
+++ engine/systems/RenderSystem.cpp
@@ -34,11 +34,13 @@ void RenderSystem::drawRect(SDL_Renderer *renderer,
 }
 
 void RenderSystem::drawSprite(SDL_Renderer *renderer,
+                              AssetManager &assets,
                               const Transform &transform,
                               const Sprite &sprite,
                               const Camera2D &camera)
 {
-    if (!sprite.texture || !sprite.texture->get())
+    Texture *texture = assets.getTexture(sprite.texturePath);
+    if (!texture)
     {
         return;
     }
@@ -50,7 +52,7 @@ void RenderSystem::drawSprite(SDL_Renderer *renderer,
 
     SDL_RenderCopy(
         renderer,
-        sprite.texture->get(),
+        texture->get(),
         nullptr,
         &dst);
 }
