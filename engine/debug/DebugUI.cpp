a30e7196e734c98f1eff325b4c23f599e49a93fd
Otavio Capilla <otavio.reis-ext@ab-inbev.com>
2026-02-04 11:13:20 -0300

    refactor: improve code readability in DebugUI by adjusting formatting and spacing

--- engine/debug/DebugUI.cpp
+++ engine/debug/DebugUI.cpp
@@ -1,14 +1,18 @@
 #include "DebugUI.h"
 
 #include <imgui.h>
+#include <engine/debug/ECSInspector.h>
 #include <backends/imgui_impl_sdl2.h>
 #include <backends/imgui_impl_sdlrenderer2.h>
 
-static SDL_Renderer* s_renderer = nullptr;
+static SDL_Renderer *s_renderer = nullptr;
 static bool s_initialized = false;
 
-void DebugUI::init(SDL_Window* window, SDL_Renderer* renderer)
+void DebugUI::init(SDL_Window *window, SDL_Renderer *renderer)
 {
+    if (!s_initialized)
+        return;
+
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGui::StyleColorsDark();
@@ -22,7 +26,8 @@ void DebugUI::init(SDL_Window* window, SDL_Renderer* renderer)
 
 void DebugUI::shutdown()
 {
-    if (!s_initialized) return;
+    if (!s_initialized)
+        return;
 
     ImGui_ImplSDLRenderer2_Shutdown();
     ImGui_ImplSDL2_Shutdown();
@@ -34,16 +39,18 @@ void DebugUI::shutdown()
 
 void DebugUI::beginFrame()
 {
-    if (!s_initialized) return;
+    if (!s_initialized)
+        return;
 
     ImGui_ImplSDLRenderer2_NewFrame();
     ImGui_ImplSDL2_NewFrame();
     ImGui::NewFrame();
 }
 
-void DebugUI::draw(World& world)
+void DebugUI::draw(World &world)
 {
-    if (!s_initialized) return;
+    if (!s_initialized)
+        return;
 
     ImGui::Begin("Debug");
     ImGui::Text("Transforms: %zu", world.transforms.size());
@@ -54,15 +61,17 @@ void DebugUI::draw(World& world)
     ImGui::Text("Camera Offsets:    %zu", world.cameraOffsets.size());
     ImGui::Text("Input Intent:    %zu", world.inputIntents.size());
     ImGui::End();
+
+    ECSInspector::draw(world);
 }
 
 void DebugUI::endFrame()
 {
-    if (!s_initialized) return;
+    if (!s_initialized)
+        return;
 
     ImGui::Render();
     ImGui_ImplSDLRenderer2_RenderDrawData(
         ImGui::GetDrawData(),
-        s_renderer
-    );
+        s_renderer);
 }
